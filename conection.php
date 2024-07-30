fdc66778ace418ec58668f29a7d69114464bef35
ROBINSON00DARIO <s71200siemens@gmail.com>
2024-07-30 14:06:47 -0500

    Add files via upload

---
+++ conection.php
@@ -0,0 +1,4 @@
+<?php
+	$conexion=mysqli_connect("bwyejcc9clzgwx01hou8-mysql.services.clever-cloud.com", "ugheukfmgktpxlar","ZrObfJdsxIQz9LJil86j", "bwyejcc9clzgwx01hou8");
+	mysqli_set_charset($conexion, "utf8");
+
